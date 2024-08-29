4714a141f5bf1dc87fc0d58bce38ec58ae417d10
Joe Bautista <jomelgbautista@gmail.com>
2024-08-29 10:43:12 -0700

    updated test files

--- portfolio-project/backend/routes/exerciseRoutes.js
+++ portfolio-project/backend/routes/exerciseRoutes.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const exerciseController = require('../controllers/exerciseController');
+const express = require("express")
+const exerciseController = require("../controllers/exerciseController")
 
-const router = express.Router();
+const router = express.Router()
 
-router.post('/exercises', exerciseController.createExercise);
-router.get('/exercises', exerciseController.getExercises);
-router.get('/exercises/:id', exerciseController.getExerciseById);
-router.put('/exercises/:id', exerciseController.updateExerciseById);
-router.delete('/exercises/:id', exerciseController.deleteExerciseById);
+router.post("/exercises", exerciseController.createExercise)
+router.get("/exercises", exerciseController.getExercises)
+router.get("/exercises/:id", exerciseController.getExerciseById)
+router.put("/exercises/:id", exerciseController.updateExerciseById)
+router.delete("/exercises/:id", exerciseController.deleteExerciseById)
 
-module.exports = router;
+module.exports = router
--- portfolio-project/backend/server.js
+++ portfolio-project/backend/server.js
@@ -1,25 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const exerciseRoutes = require('./routes/exerciseRoutes');
+const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+const exerciseRoutes = require("./routes/exerciseRoutes")
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-mongoose.connect(process.env.MONGODB_CONNECT_STRING, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('MongoDB connection error:', error);
-});
+mongoose
+  .connect(process.env.MONGODB_CONNECT_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB")
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error)
+  })
 
-app.use('/api', exerciseRoutes);
+app.use("/api", exerciseRoutes)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+  console.log(`Server running on port ${port}`)
+})
